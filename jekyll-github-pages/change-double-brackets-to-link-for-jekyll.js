e34948c5d400cd371979c0e114843222380cedd8
woodtalk <woodtalk@daum.net>
2020-11-11 01:59:22 +0900

    link 변경 header 에서 호출 가능하도록 수정

--- jekyll-github-pages/change-double-brackets-to-link-for-jekyll.js
+++ jekyll-github-pages/change-double-brackets-to-link-for-jekyll.js
@@ -1,5 +1,12 @@
-;(function() {
-    var content = document.querySelector('article.post-content');
-    content.innerHTML = content.innerHTML.replace(/\[\[(.+?)\]\]\{(.+?)\}/g, '<a href="wiki/$1">$2</a>');
-    content.innerHTML = content.innerHTML.replace(/\[\[(.+?)\]\]/g, '<a href="wiki/$1">$1</a>');
-})();
+document.addEventListener('DOMContentLoaded', () => {
+	(function t() {
+		var content = document.querySelector('article.post-content');
+		if (content.innerHTML.length == 0) {
+			setTimeout(t, 1);
+			return;
+		}
+
+		content.innerHTML = content.innerHTML.replace(/\[\[(.+?)\]\]\{(.+?)\}/g, '<a href="wiki/$1">$2</a>');
+		content.innerHTML = content.innerHTML.replace(/\[\[(.+?)\]\]/g, '<a href="wiki/$1">$1</a>');
+	})();
+});
